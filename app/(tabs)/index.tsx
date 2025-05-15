a31bf65937d20798111d5631a84f05461411770f
asino-nelson <asinonelson276@gmail.com>
2025-05-15 21:23:00 +0300

    Account actions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import ActionItems from '@/components/home/ActionItems';
 import WelcomeUser from '@/components/home/WelcomeUser';
 import { Text, View } from 'react-native';
 
@@ -5,6 +6,7 @@ export default function HomeScreen() {
   return (
     <View className='p-5 flex-1'>
       <WelcomeUser />
+      <ActionItems />
 
 
     </View>
---
+++ components/home/ActionItems.tsx
@@ -0,0 +1,30 @@
+import { View, Text, FlatList } from 'react-native'
+import React from 'react'
+import data from '../../assets/data.json'
+import { Ionicons } from '@expo/vector-icons'
+
+
+const ActionItems = () => {
+  return (
+    <FlatList
+      data={data.actions}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={(item) => item.name}
+      contentContainerStyle={{
+        width: '100%',
+        paddingVertical: 10,
+        alignItems: 'start',
+        justifyContent: 'space-between',
+      }}
+      renderItem={({ item }) => (
+        <View className='bg-white w-24 h-24 p-2 items-center justify-center rounded-full mb-4'>
+          <Ionicons name={item.icon} size={24} color="black" />
+          <Text className='text-base font-semibold'>{item.name}</Text>
+        </View>
+      )}
+    />
+  )
+}
+
+export default ActionItems
