2803b2c670228530cba275749c48be6a1a720cd8
asino-nelson <asinonelson276@gmail.com>
2025-05-15 22:03:06 +0300

    Actions debug

--- components/home/ActionItems.tsx
+++ components/home/ActionItems.tsx
@@ -6,24 +6,27 @@ import { Ionicons } from '@expo/vector-icons'
 
 const ActionItems = () => {
   return (
-    <FlatList
-      data={data.actions}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      keyExtractor={(item) => item.name}
-      contentContainerStyle={{
-        width: '100%',
-        paddingVertical: 10,
-        alignItems: 'start',
-        justifyContent: 'space-between',
-      }}
-      renderItem={({ item }) => (
-        <View className='bg-white w-24 h-24 p-2 items-center justify-center rounded-full mb-4'>
-          <Ionicons name={item.icon} size={24} color="black" />
-          <Text className='text-base font-semibold'>{item.name}</Text>
-        </View>
-      )}
-    />
+    <View>
+      <FlatList
+        data={data.actions}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{
+          width: '100%',
+          paddingVertical: 5,
+          alignItems: 'start',
+          justifyContent: 'space-between',
+        }}
+        renderItem={({ item }) => (
+          <View className='bg-white w-24 h-24 p-2 items-center justify-center rounded-full'>
+            <Ionicons name={item.icon} size={24} color="black" />
+            <Text className='text-base font-semibold'>{item.name}</Text>
+          </View>
+        )}
+      />
+    </View>
+
   )
 }
 
